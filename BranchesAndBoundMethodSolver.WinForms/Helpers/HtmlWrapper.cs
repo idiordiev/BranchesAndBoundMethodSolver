2b7774f91665ecd357d910504d8cfa9f81400704
Danil Dukhovnyi <duhovny.daniil@gmail.com>
2023-11-13 19:38:19 +0200

    Add numering to html

--- BranchesAndBoundMethodSolver.WinForms/Helpers/HtmlWrapper.cs
+++ BranchesAndBoundMethodSolver.WinForms/Helpers/HtmlWrapper.cs
@@ -12,6 +12,7 @@ namespace BranchesAndBoundMethodSolver.WinForms.Helpers
             stringBuilder.Append("<table>\n" +
                                  " <thead>\n" +
                                  "     <tr>\n" +
+                                 "         <th scope=\"col\">&nbsp;\u2116</th>\n" +
                                  "         <th scope=\"col\"><span style=\"\">Підмножина</span></th>\n" +
                                  "         <th scope=\"col\"><span style=\"\">Оцінка</span></th>\n" +
                                  "         <th scope=\"col\"><span style=\"\">Статус</span></th>\n" +
@@ -19,9 +20,11 @@ namespace BranchesAndBoundMethodSolver.WinForms.Helpers
                                  " </thead>\n" +
                                  " <tbody>\n");
 
+            int counter = 1;
             foreach (Node? node in nodes)
             {
                 stringBuilder.Append("     <tr>\r\n" +
+									 $"         <td>&nbsp;{counter++}</td>\n" +
                                      "         <td>{1:SA:=" + $"{node.Path}" + "}</td>\n" +
                                      "         <td>{1:SA:=" + $"{node.Cost}" + "}</td>\n");
 
